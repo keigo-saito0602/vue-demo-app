9467d60c066ea0773c10e1ce28e14583a2621dc9
keigo-saito0602 <keigo.saito@vitalize.co.jp>
2025-03-24 15:20:18 +0900

    :zap: コンポーネントの形式を変更

--- src/components/UsernameDisplay.vue
+++ src/components/UsernameDisplay.vue
@@ -1,25 +1,26 @@
 <template>
   <div>
-    <p v-if="username">{{ $t('app.hello-title', { username }) }}</p>
-    <p v-if="isSubscribed">{{$t("checkbox.check")}}</p>
-    <p v-else>{{$t("checkbox.checknull")}}</p>
+    <p v-if="username">{{ $t("app.hello-title", { username }) }}</p>
+    <p v-if="isSubscribed">{{ $t("checkbox.check") }}</p>
+    <p v-else>{{ $t("checkbox.checknull") }}</p>
   </div>
 </template>
 
 <script>
-export default {
-  props: {
-    username: String,
-    isSubscribed: Boolean, // チェックボックスの状態を受け取る
-  },
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component({ name: "UsernameDisplay" })
+export default class UsernameDisplay extends Vue {
+  @Prop(String) username;
+  @Prop(Boolean) isSubscribed;
 }
 </script>
 
 <style scoped>
-  .greeting {
-    font-size: 20px;
-    font-weight: bold;
-    color: #007BFF;
-    margin-top: 20px;
-  }
+.greeting {
+  font-size: 20px;
+  font-weight: bold;
+  color: #007bff;
+  margin-top: 20px;
+}
 </style>
