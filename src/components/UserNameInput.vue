c04febc62122ef5d07fe8de78c1f325d6ee132df
Keigo <136228606+keigo-saito0602@users.noreply.github.com>
2025-03-24 15:46:45 +0900

    :zap: i18の対応、コンポーネントの形式変更、npm run devが使用できるように追加 (#4)
    * 国際化ファイルの修正
    
    * 古い国際化ファイルの削除
    
    * 国際化ファイルのコンポーネントを適応
    
    * VueI18nno
    
    * :zap: コンポーネントの形式を変更
    
    * npm run devが使えるようにする
    
    * :bug: usernameが正しく表示されない問題の修正

--- src/components/UserNameInput.vue
+++ src/components/UserNameInput.vue
@@ -1,70 +1,69 @@
 <template>
   <div>
-    <label for="username">Enter your username:</label>
-    <input class="username-form" type="text" id="username" v-model="inputUsername" /><br>
-    <button @click="submitUsername">Submit</button><br>
+    <label for="username">{{ $t("app.title") }}</label>
+    <input
+      class="username-form"
+      type="text"
+      id="username"
+      v-model="inputUsername"
+    /><br />
+    <button @click="submitUsername">{{ $t("button.submit") }}</button><br />
     <label>
-      <input type="checkbox" v-model="isSubscribed" /> Subscribe to updates<br>
+      <input type="checkbox" v-model="isSubscribed" /> {{ $t("checkbox.title")
+      }}<br />
     </label>
   </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      inputUsername: '',
-      isSubscribed: false, // チェックボックスの初期状態
-    };
-  },
-  methods: {
-    submitUsername() {
-      // チェックボックスの状態も親コンポーネントに送信
-      this.$emit('username-submitted', this.inputUsername, this.isSubscribed);
-    },
-  },
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({ name: "UserNameInput" })
+export default class UserNameInput extends Vue {
+  inputUsername = "";
+  isSubscribed = false;
+
+  submitUsername() {
+    this.$emit("username-submitted", this.inputUsername, this.isSubscribed);
+  }
 }
 </script>
 
 <style scoped>
-  label {
-    font-weight: bold;
-    margin-right: 10px;
-  }
+label {
+  font-weight: bold;
+  margin-right: 10px;
+}
 
-  input {
-    padding: 5px;
-  }
+input {
+  padding: 5px;
+}
 
-  button {
-    background-color: #007BFF;
-    color: black;
-    margin: 10px 5px;
-    padding: 5px 30px;
-    border: none;
-    cursor: pointer;
-    /*影の基点とするためrelativeを指定*/
-    position: relative;
-    /*ボタンの形状*/
-    text-decoration: none;
-    display: inline-block;
-    text-align: center;
-    background: transparent;
-    border-radius: 25px;
-    border: solid 1px #333;
-    outline: none;
-    /*アニメーションの指定*/
-    transition: all 0.3s ease;
-  }
+button {
+  background-color: #007bff;
+  color: black;
+  margin: 10px 5px;
+  padding: 5px 30px;
+  border: none;
+  cursor: pointer;
+  position: relative;
+  text-decoration: none;
+  display: inline-block;
+  text-align: center;
+  background: transparent;
+  border-radius: 25px;
+  border: solid 1px #333;
+  outline: none;
+  transition: all 0.3s ease;
+}
 
-  button:hover {
-    background-color: #0056b3;
-    color:white;
-    border-color:transparent;
-  }
+button:hover {
+  background-color: #0056b3;
+  color: white;
+  border-color: transparent;
+}
 
-
-  username-form {
-    margin: 10px 10px;
-  }
+.username-form {
+  margin: 10px 10px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueI18n from 'vue-i18n';
+
+// 言語の設定
+Vue.use(VueI18n);
+
+const i18n = new VueI18n({
+  locale: 'ja', // ← ここを「en」に切り替えれば英語になる
+  fallbackLocale: 'ja',
+  messages: {
+    ja : require('./locales/ja.json'),
+    en : require('./locales/en.json')
+  }
+});
 
 Vue.config.productionTip = false
 
 new Vue({
+  i18n,
   render: h => h(App),
 }).$mount('#app')
