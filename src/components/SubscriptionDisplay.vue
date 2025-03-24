4e502400fa8aeb0c3ee7b929928dc30f7b66e36a
keigo-saito0602 <keigo.saito@vitalize.co.jp>
2025-03-24 16:51:35 +0900

    :zap: ライフサイクルの機能を追加

---
+++ src/components/SubscriptionDisplay.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <p v-if="isSubscribed">{{ $t("checkbox.check") }}</p>
+    <p v-else>{{ $t("checkbox.checknull") }}</p>
+  </div>
+</template>
+
+<script>
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component({ name: "SubscriptionDisplay" })
+export default class SubscriptionDisplay extends Vue {
+  @Prop({ type: Boolean, default: false }) isSubscribed;
+
+  // コンポーネントがインスタンス化された直後に呼ばれる
+  created() {
+    console.log(
+      "SubscriptionDisplay.vue: created() - コンポーネントがインスタンス化されました"
+    );
+  }
+}
+</script>
+
+<style scoped>
+.greeting {
+  font-size: 20px;
+  font-weight: bold;
+  color: #007bff;
+  margin-top: 20px;
+}
+</style>
