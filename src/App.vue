4d0fd74f880ff158c86c4455a64885703fd71197
keigo-saito0602 <keigo.saito@vitalize.co.jp>
2025-04-01 09:44:36 +0900

    :wrench: sass Web Font Loader を追加

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
@@ -38,16 +34,16 @@
     </v-app-bar>
 
     <v-main>
-      <router-view/>
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import Vue from "vue";
 
 export default Vue.extend({
-  name: 'App',
+  name: "App",
 
   data: () => ({
     //
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import VueI18n from "vue-i18n";
 import "./assets/styles/global.css"; // グローバルCSS
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify'
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
 
 // 言語の設定
 Vue.use(VueI18n);
@@ -25,5 +25,5 @@ new Vue({
   router,
   store,
   vuetify,
-  render: (h) => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
